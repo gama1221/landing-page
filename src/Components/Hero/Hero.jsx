57723927b44c499535b3bc1cd6bc9a7989143f2c
getinet amare <getinetamare@gmail.com>
2023-07-14 16:40:26 +0300

    Initial commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import Typed from "react-typed";
+
+const Hero=()=>{
+    return(
+        <section>
+            <div>
+                <article className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+                    <p className="text-purple-900">
+                       GROWING WITH DATA ANALYTICS
+                    </p>
+                    <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6"> Grow wth data</h1>
+                    <div>
+                        <p className="md:text-5xl sm:text-xl font-bold">Fast, flexible finance for</p>
+                        
+                    </div>
+                </article>
+            </div>            
+        </section>
+    )
+}
+export default Hero;
